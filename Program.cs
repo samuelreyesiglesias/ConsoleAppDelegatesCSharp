c75c704da3cfab42cdc640019b975c587e56db0c
samuelreyesiglesias <samuelreyesiglesias@gmail.com>
2024-08-21 15:04:04 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppDelegatesCSharp
+{
+    class Program
+    {
+
+        public delegate void test(string nombre);
+        delegate int delegadoSumar(int num1, int num2);
+        static void Main(string[] args)
+        {
+            //Ejemplo delegado 1, configado desde la propiedad de tipo de delegado que puede asignarsele un metodo en su constructor
+            //realizando una instancia como si se trata de un objeto.-
+            test test1 = new test(muestraMensaje); 
+            test1("Samuel");
+
+
+            //Ejemplo 2: Creamos la propiedad de tipo delegado
+            //como estamos en una clase estatica debemos tambien crear los metodos de tipo static
+            delegadoSumar metodoSumar = new delegadoSumar(sumar);
+            int sumatoria = metodoSumar(1, 3);
+
+
+        }
+        
+        public static void muestraMensaje(string nombre)
+        {
+            Console.WriteLine($"Hola {nombre}");
+        }
+
+        public static int sumar(int num1 , int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
